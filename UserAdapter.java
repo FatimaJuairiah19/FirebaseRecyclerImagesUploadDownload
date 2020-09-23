82540b4827b2bc817fcee82a96acf6d02e1fe83c
FatimaJuairiah19 <60512514+FatimaJuairiah19@users.noreply.github.com>
2020-09-23 21:43:27 +0600

    Add files via upload

---
+++ UserAdapter.java
@@ -0,0 +1,73 @@
+package com.example.uploadandretrivephoto;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.ViewHolder> {
+
+    private Context mContext;
+    private List<User> mUsers;
+    private boolean ischat;
+
+    String theLastMessage;
+
+    public UserAdapter(Context mContext, List<User> mUsers) {
+        this.mUsers = mUsers;
+        this.mContext = mContext;
+
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.user_item, parent, false);
+        return new UserAdapter.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+
+        final User user = mUsers.get(position);
+        holder.username.setText(user.getUsername());
+        if (user.getImageURL().equals("default")) {
+            holder.profile_image.setImageResource(R.mipmap.ic_launcher);
+        }
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsers.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        public TextView username;
+        public ImageView profile_image;
+        private ImageView img_on;
+        private ImageView img_off;
+        private TextView last_msg;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            username = itemView.findViewById(R.id.username);
+            profile_image = itemView.findViewById(R.id.profile_image);
+
+        }
+
+        //check for last message
+
+
+    }
+}
